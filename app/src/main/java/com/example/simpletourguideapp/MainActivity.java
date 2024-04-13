e7cd1ae5ee3a80847f8b3d300991346388122d28
Reymar Villanueva <reymarfactor@gmail.com>
2024-04-13 15:23:40 +0800

    add ics and activity_main.xml

--- app/src/main/java/com/example/simpletourguideapp/MainActivity.java
+++ app/src/main/java/com/example/simpletourguideapp/MainActivity.java
@@ -12,16 +12,11 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private CardView ics, coeng, cbpa, cas;
-    private ImageView imageView;
-    private Button buttonDisplayImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        imageView = findViewById(R.id.image_view);
-        buttonDisplayImage = findViewById(R.id.button_display_image);
 
         ics = findViewById(R.id.ics);
         coeng = findViewById(R.id.coeng);
@@ -32,26 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         coeng.setOnClickListener(this);
         cbpa.setOnClickListener(this);
         cas.setOnClickListener(this);
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Hide the ImageView when clicked
-                imageView.setVisibility(View.INVISIBLE);
-            }
-        });
-
-        // Set OnClickListener to the Button
-        buttonDisplayImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Show the ImageView when the button is clicked
-                imageView.setVisibility(View.VISIBLE);
-                // Set the image resource to the ImageView
-                imageView.setImageResource(R.drawable.ics); // Replace "your_image_name" with the actual name of your image
-            }
-        });
     }
+
 
     @Override
     public void onClick(View v) {
@@ -60,21 +37,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.ics:
                 // Handle click for ICS CardView
-                intent = new Intent(MainActivity.this, ics.class); // Assuming IcsActivity is the correct activity class
+                intent = new Intent(MainActivity.this, ics.class);
                 startActivity(intent);
                 break;
             case R.id.coeng:
                 // Handle click for COENG CardView
-                intent = new Intent(MainActivity.this, coeng.class); // Assuming CoengActivity is the correct activity class
+                intent = new Intent(MainActivity.this, coeng.class);
                 startActivity(intent);
                 break;
             case R.id.cbpa:
                 // Handle click for CBPA CardView
-                intent = new Intent(MainActivity.this, cbpa.class); // Assuming CbpaActivity is the correct activity class
+                intent = new Intent(MainActivity.this, cbpa.class);
                 startActivity(intent);
                 break;
             case R.id.cas:
-                intent = new Intent(MainActivity.this, cas.class); // Assuming CasActivity is the correct activity class
+
+                intent = new Intent(MainActivity.this, cas.class);
                 startActivity(intent);
                 break;
             default:
